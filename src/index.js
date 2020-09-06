aab9402ba43d0ee42ccda688175e28fe29a03df4
takahiro-bigwest <t.work.430.taka@gmail.com>
2020-09-06 23:54:15 +0900

    時計のロジック作成

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
+function  tick() {
+  const element = (
+    <div>
+      <h1>Hello,world!</h1>
+       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
 }
+setInterval(tick, 1000);
 
-const user = {
-  firstName: 'Harper',
-  lastName: 'Perez'
-};
-
-const element = (
-  <h1>
-    Hello, {formatName(user)}!
-  </h1>
-);
-
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   element,
+//   document.getElementById('root')
+// );
