cfb27003d283ee7e15b6f36035fe195b6eb93e5b
takahiro-bigwest <t.work.430.taka@gmail.com>
2020-09-06 23:50:09 +0900

    JSXの導入完了

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const name = 'Josh Perez';
-const element = <h1>Hello, {name}</h1>
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
 
 ReactDOM.render(
   element,
