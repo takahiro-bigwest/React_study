251feaa6f53476d7ede4db7befa8c8efdcaa73a1
takahiro-bigwest <t.work.430.taka@gmail.com>
2020-09-08 14:58:00 +0900

    ボタンのイベント処理実装

--- src/index.js
+++ src/index.js
@@ -1,65 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// function Clock(props) {
-//   return (
-//     <div>
-//       <h1>Helo, world!</h1>
-//       <h2>It is {props.date.toLocaleTimeString()}.</h2>
-//     </div>
-//   );
-// }
-
-// React.Component を継承する同名の ES6 クラスを作成する。
-// render() と呼ばれる空のメソッドを 1 つ追加する。
-// 関数の中身を render() メソッドに移動する。
-// render() 内の props を this.props に書き換える。
-// 空になった関数の宣言部分を削除する。
-
-class Clock extends React.Component {
+class Toggle extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {date: new Date()};
+    this.state = {isToggleOn: true};
+
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-
-  componentWillMount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
   }
 
   render() {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
     );
   }
 }
 
-function App() {
-  return (
-    <div>
-      <Clock />
-      <Clock />
-      <Clock />
-    </div>
-  );
-}
-
 ReactDOM.render(
-  <App />,
+  <Toggle />,
   document.getElementById('root')
 );
